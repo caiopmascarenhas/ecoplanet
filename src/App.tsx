8f32b3bb9ab0771ff75db20312fed6d59111841f
caiopmascarenhas <caio.pereira1998@hotmail.com>
2020-10-30 15:34:52 -0300

    Implementação do Icon e Splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes';
 import { StatusBar } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" translucent={true} backgroundColor={'transparent'} />
